b3ea3390b7e6e4b87b1e72a194d325d1c111beb9
Kurt A. Seiffert <seiffert@iu.edu>
2020-01-28 09:47:00 -0500

    Added checks for users and groups.

--- ac_and_filesystem/prep_hw5.sh
+++ ac_and_filesystem/prep_hw5.sh
@@ -3,6 +3,20 @@
 # This is a script to prep a standard Vagrant ubuntu/xenial64 image
 # for Homework 4 of I230.
 
+# Verify the users and groups have been created
+for USER in "vagrant" "student1" "student2" "instructor"; do
+    if ! id -u $USER  >/dev/null 2>&1; then
+        echo "It doesn't look like $USER was successfully added."
+        echo "Make sure your adduser command did not emit errors."
+    fi
+done
+
+for GROUP in "ta" "students"; do
+    if ! id -g $GROUP > /dev/null 2>&1; then
+        echo "Unable to locate group $GROUP. Was it successfully created?"
+    fi
+done
+
 
 # Create a directory tree for showing permissions
 sudo mkdir /usr/local/projects
@@ -17,9 +31,9 @@ sudo chown student2 /usr/local/projects/stud_proj2
 
 
 # change the group of the student projects to ta
-sudo chgrp ta /usr/local/projects/stud_proj?
+sudo chgrp ta /usr/local/projects/stud_proj
 
 # Allow the owners of the student project directory have full permissions as with the ta's
-sudo chmod u+rwx,g+rwx,o-rwx /usr/local/projects/stud_proj?
+sudo chmod u+rwx,g+rwx,o-rwx /usr/local/projects/stud_proj
 
 
