e25fdc6f994a68f31247f9540052f87ffa0a2447
Kurt A. Seiffert <seiffert@iu.edu>
2020-02-11 08:48:03 -0500

    Added demo for process security

---
+++ lectures/processes/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Simple program for demonstration purposes
+
+echo Hello World
+env | grep HELLO
---
+++ lectures/processes/sneaky.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Sneaky program to redact some of hello.sh output
+
+/vagrant/processes/hello.sh |grep -v HELLO
